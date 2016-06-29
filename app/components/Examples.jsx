ca88bc282104e1b69f70e66d4e9d96c59f7de036
Laura Vingada <laura@vingada.com>
2016-06-29 19:27:22 -0400

    Style the Examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,18 +1,28 @@
 var React = require('react');
-
-// var Examples = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>Examples Component</h3>
-//     )
-//   }
-// });
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to Examples page!</p>
+      <h1 className='text-center'>Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Toronto'>Toronto, Canada</Link>
+        </li>
+        <li>
+          <Link to='/?location=Belo+Horizonte'>Belo Horizonte, Brazil</Link>
+        </li>
+        <li>
+          <Link to='/?location=London,uk'>London, UK</Link>
+        </li>
+        <li>
+          <Link to='/?location=Sydney,au'>Sydney, Australia</Link>
+        </li>
+        <li>
+          <Link to='/?location=Phuket'>Phuket, Thailand</Link>
+        </li>
+      </ol>
     </div>
   )
 };
