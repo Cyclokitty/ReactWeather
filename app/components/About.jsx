76b15cd2ffdd9d7bf11b3d82c137b619ab1b4bfc
Laura Vingada <laura@vingada.com>
2016-06-29 15:54:04 -0400

    Update the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,13 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <h4>Welcome to the About Page</h4>
+      <p>Hi, I'm Laura and this is the weather app I made in the Udemy "The Complete React Web App Developer Course".</p>
+      <p>This app is made with Node, React, and kept up to date on Github, and deployed on Heroku.</p>
+      <p>As I learn more, this weather app will be more and more awesome.</p>
+    </div>
   )
 };
 
