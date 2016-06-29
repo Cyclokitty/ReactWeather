2f5f3169f7cb508dba2b17cded045615335d73f6
Laura Vingada <laura@vingada.com>
2016-06-29 19:52:54 -0400

    Add styling to About page and links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,20 +1,21 @@
 var React = require('react');
-
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <h4>Welcome to the About Page</h4>
-      <p>Hi, I'm Laura and this is the weather app I made in the Udemy "The Complete React Web App Developer Course".</p>
-      <p>This app is made with Node, React, and kept up to date on Github, and deployed on Heroku.</p>
+      <h1 className='text-center'>About</h1>
+      <h4 className='text-center'>Welcome to the About Page</h4>
+      <p>Hi, I'm Laura and this is the weather app I made in the <Link to='https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/overview'> Udemy "The Complete React Web App Developer Course"</Link>.</p>
+      <p>This app is made with:
+        <ul>
+          <li><Link to='https://nodejs.org/en/'>Node,</Link></li>
+          <li><Link to='https://facebook.github.io/react/'>React,</Link></li>
+          <li><Link to='http://foundation.zurb.com/'>Foundation,</Link></li>
+          <li><Link to='https://github.com/Cyclokitty/ReactWeather'>kept up to date on Github,</Link></li>
+          <li><Link to='http://quiet-badlands-22529.herokuapp.com/'>and deployed on Heroku.</Link></li>
+        </ul>
+        </p>
       <p>As I learn more, this weather app will be more and more awesome.</p>
     </div>
   )
