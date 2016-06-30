426a32a30fcfb0698210c063731777390db51369
Laura Vingada <laura@vingada.com>
2016-06-29 20:06:37 -0400

    Correct Link tags to a href tags in About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,11 +9,11 @@ var About = (props) => {
       <p>Hi, I'm Laura and this is the weather app I made in the <Link to='https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/overview'> Udemy "The Complete React Web App Developer Course"</Link>.</p>
       <p>This app is made with:
         <ul>
-          <li><Link to='https://nodejs.org/en/'>Node,</Link></li>
-          <li><Link to='https://facebook.github.io/react/'>React,</Link></li>
-          <li><Link to='http://foundation.zurb.com/'>Foundation,</Link></li>
-          <li><Link to='https://github.com/Cyclokitty/ReactWeather'>kept up to date on Github,</Link></li>
-          <li><Link to='http://quiet-badlands-22529.herokuapp.com/'>and deployed on Heroku.</Link></li>
+          <li><a href='https://nodejs.org/en/'>Node,</a></li>
+          <li><a href='https://facebook.github.io/react/'>React,</a></li>
+          <li><a href='http://foundation.zurb.com/'>Foundation,</a></li>
+          <li><a href='https://github.com/Cyclokitty/ReactWeather'>kept up to date on Github,</a></li>
+          <li><a href='http://quiet-badlands-22529.herokuapp.com/'>and deployed on Heroku.</a></li>
         </ul>
         </p>
       <p>As I learn more, this weather app will be more and more awesome.</p>
